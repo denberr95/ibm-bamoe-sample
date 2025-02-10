c8b592f44dfe94e8d7c2ade8223bd3ab3c9483dc
Denis Berretti <denis.berretti95@gmail.com>
2025-02-10 16:20:03 +0100

    Fix missing dependencies

--- business-application-service/src/main/java/com/pam/sample/BPMApplication.java
+++ business-application-service/src/main/java/com/pam/sample/BPMApplication.java
@@ -2,8 +2,10 @@ package com.pam.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@EnableAspectJAutoProxy
 @EnableWebSecurity
 @SpringBootApplication
 public class BPMApplication {
