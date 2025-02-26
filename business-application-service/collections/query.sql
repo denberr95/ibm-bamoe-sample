30a08a40a79e0cf2646545e3334f96e7ac4aa8f2
Denis Berretti <denis.berretti95@gmail.com>
2025-02-26 14:17:04 +0100

    Aggiunte query per svecchiamento

--- business-application-service/collections/query.sql
+++ business-application-service/collections/query.sql
@@ -18,3 +18,93 @@ from qrtz_triggers;
 
 select *
 from qrtz_simple_triggers;
+
+-- ErrorInfo 
+select errorinfo0_.id as col_0_0_ 
+from ErrorInfo errorinfo0_
+where errorinfo0_.timestamp <= ?
+
+delete from ErrorInfo 
+where id in (?, ?);
+
+-- RequestInfo
+select requestinf0_.id as col_0_0_ 
+from RequestInfo requestinf0_ 
+where requestinf0_.status in ('ERROR', 'CANCELLED', 'DONE')
+and requestinf0_.timestamp <= ?;
+
+delete from RequestInfo
+where id in (?, ?);
+
+-- AuditTaskImpl
+select audittaski0_.id as col_0_0_ 
+from AuditTaskImpl audittaski0_ 
+where audittaski0_.processInstanceId in (
+    select processins1_.processInstanceId
+    from ProcessInstanceLog processins1_
+    where processins1_.status in (2, 3)
+);
+
+delete from AuditTaskImpl
+where id in (?, ?);
+
+-- TaskEvent
+select taskeventi0_.id as col_0_0_
+from TaskEvent taskeventi0_ 
+where taskeventi0_.createdOn <= ?
+and taskeventi0_.processInstanceId in (
+    select processins1_.processInstanceId 
+    from ProcessInstanceLog processins1_ 
+    where processins1_.status in (2, 3)
+);
+
+delete from TaskEvent 
+where id in (?, ?);
+
+-- TaskVariableImpl
+select taskvariab0_.id as col_0_0_
+from TaskVariableImpl taskvariab0_ 
+where taskvariab0_.modificationDate <= ?
+and taskvariab0_.processInstanceId in (
+    select processins1_.processInstanceId 
+    from ProcessInstanceLog processins1_ 
+    where processins1_.status in (2, 3)
+);
+
+delete from TaskVariableImpl 
+where id in (?, ?);
+
+-- NodeInstanceLog
+select nodeinstan0_.id as col_0_0_ 
+from NodeInstanceLog nodeinstan0_ 
+where nodeinstan0_.log_date <= ?
+and nodeinstan0_.processInstanceId in (
+    select processins1_.processInstanceId 
+    from ProcessInstanceLog processins1_ 
+    where processins1_.status in (2, 3)
+);
+
+delete from NodeInstanceLog 
+where id in (?, ?);
+
+-- VariableInstanceLog
+select variablein0_.id as col_0_0_ 
+from VariableInstanceLog variablein0_ 
+where variablein0_.log_date <= ? 
+and variablein0_.processInstanceId in (
+    select processins1_.processInstanceId 
+    from ProcessInstanceLog processins1_ 
+    where processins1_.status in (2, 3)
+);
+
+delete from VariableInstanceLog 
+where id in (?, ?);
+
+-- ProcessInstanceLog
+select processins0_.id as col_0_0_ 
+from ProcessInstanceLog processins0_ 
+where processins0_.status in (2, 3)
+and processins0_.end_date <= ?;
+
+delete from ProcessInstanceLog 
+where id in (?, ?);
